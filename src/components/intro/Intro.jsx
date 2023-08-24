b76ccf301bce349b3e50db909383966d083f908b
Francisco Andrade <andradef801@gmail.com>
2023-08-24 13:27:12 -0600

    App initiated and started work on intro

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./intro.css"
+
+const Intro = () => {
+  return (
+    <div>
+      <div className="i">
+        <div className="i-left">left</div>
+        <div className="i-right">right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
